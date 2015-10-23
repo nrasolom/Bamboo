947e6e7c0eb2ae7ecf5477b4c34ed012f4930183
Loïc <ldivad@isep.fr>
2015-10-23 16:25:49 +0200

    Vectorisation of getters
    + Bamboo class (util)
    + Fix the NULL result belongs to the time_zone issue
    • change the return format of getday

--- src/main/java/org/isep/pandas/udf/Day.java
+++ src/main/java/org/isep/pandas/udf/Day.java
@@ -6,23 +6,12 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 
 public class Day extends UDF {
-	private final String FORMAT = "yyyy-MM-dd HH:mm:ss";
-	
+
 	public Text evaluate(Text dateTime){
-		DateTime dtt;
-		try{
-			dtt = DateTime.parse(
-					dateTime.toString(), 
-					DateTimeFormat.forPattern(this.FORMAT)
-				);
-		}catch(IllegalArgumentException e){
-			return new Text("NULL");
-		}
-		
-		String d = dtt.getYear() + 
-				 "_day-" + 
-				 dtt.getDayOfYear();
-		
+		DateTime dtt = Bamboo.dateParse(dateTime.toString());
+		if (dtt == null ) return new Text("NULL");
+		//String d = dtt.getYear() + "_day-" + dtt.getDayOfYear();
+		String d = dtt.toString("yyyy-MM-dd");
 		return new Text(d);
 	}
 }
